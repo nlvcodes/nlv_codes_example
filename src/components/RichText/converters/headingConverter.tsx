177dd2e870aa1dbb326cef2310372a8b00e56b44
nick <nick0vogel@gmail.com>
2025-01-07 17:18:46 -0500

    Adjusted heading converter

--- src/components/RichText/converters/headingConverter.tsx
+++ src/components/RichText/converters/headingConverter.tsx
@@ -4,14 +4,11 @@ import { SerializedHeadingNode } from '@payloadcms/richtext-lexical'
 
 export const headingConverter: JSXConverters<SerializedHeadingNode> = {
   heading: ({node, nodesToJSX}) => {
-    if (node.tag === 'h2') {
       const text = nodesToJSX({ nodes: node.children }).join("")
-
+    if (node.tag === 'h2') {
       const id = text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
       return <h2 id={id}>{text}</h2>
-    }
-    else {
-      const text = nodesToJSX({ nodes: node.children }).join("")
+    } else {
       const Tag = node.tag
       return <Tag>{text}</Tag>
     }
