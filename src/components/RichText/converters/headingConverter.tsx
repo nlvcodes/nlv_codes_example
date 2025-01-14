2a776be0b5315b2e9e954d30651531bd0a28509b
nick <nick0vogel@gmail.com>
2025-01-13 21:57:08 -0500

    rendering lexical content

--- src/components/RichText/converters/headingConverter.tsx
+++ src/components/RichText/converters/headingConverter.tsx
@@ -5,9 +5,9 @@ import { SerializedHeadingNode } from '@payloadcms/richtext-lexical'
 export const headingConverter: JSXConverters<SerializedHeadingNode> = {
   heading: ({node, nodesToJSX}) => {
     if (node.tag === 'h2') {
-      const text = nodesToJSX({ nodes: node.children }).join("")
+      const text = nodesToJSX({ nodes: node.children })
 
-      const id = text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
+      const id = text.join("").toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
       return <h2 id={id}>{text}</h2>
     }
     else {
