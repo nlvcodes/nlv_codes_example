22156d809416359c728b6481038827653f5599a0
nick <nick0vogel@gmail.com>
2025-02-22 20:32:00 -0500

    getting started with tw

--- src/blocks/ContentWithMedia/Component.tsx
+++ src/blocks/ContentWithMedia/Component.tsx
@@ -1,27 +1,18 @@
 import type { ContentWithMedia as ContentWithMediaProps } from '@/payload-types'
 import Image from 'next/image'
-import {RichText} from '@/components/RichText'
+import { RichText } from '@/components/RichText'
 
 type Props = {
   className?: string
 } & ContentWithMediaProps
 
 export const ContentWithMedia: React.FC<Props> = (block) => {
-  if (block.textPosition === 'Left') {
-    return <section>
-      {block.content && <RichText data={block.content} />}
-      {block.image && typeof block.image !== 'string' &&
-        <Image src={block.image.thumbnailURL || ``} alt={block.image.alt || ``}
-        width={block.image.width || 640} height={block.image.height || 360}
-        />}
-    </section>
-  } else if (block.textPosition === 'Right') {
-    return <section>
-      {block.image && typeof block.image !== 'string' &&
-        <Image src={block.image.thumbnailURL || ``} alt={block.image.alt || ``}
-        width={block.image.width || 640} height={block.image.height || 360}
-        />}
-      {block.content && <RichText data={block.content} />}
-    </section>
-  }
+  return <section className={'grid grid-cols-12 gap-4 m-4 p-4 bg-emerald-400 rounded-2xl'}>
+    {block.content && <RichText data={block.content} className={`col-span-3 ${block.textPosition === 'Right' ? `order-last` : `order-first`}`} />}
+    {block.image && typeof block.image !== 'string' &&
+      <Image src={block.image.thumbnailURL || ``} alt={block.image.alt || ``} className={'col-span-9'}
+             width={block.image.width || 640} height={block.image.height || 360}
+      />}
+  </section>
+
 }
