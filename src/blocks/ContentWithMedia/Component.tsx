e2367567ba7da106c84a45de6d359088896d8828
nick <nick0vogel@gmail.com>
2025-03-07 10:42:09 -0500

    finished

--- src/blocks/ContentWithMedia/Component.tsx
+++ src/blocks/ContentWithMedia/Component.tsx
@@ -7,12 +7,22 @@ type Props = {
 } & ContentWithMediaProps
 
 export const ContentWithMedia: React.FC<Props> = (block) => {
-  return <section className={'grid grid-cols-12 gap-4 m-4 p-4 bg-emerald-400 rounded-2xl'}>
-    {block.content && <RichText data={block.content} className={`col-span-3 ${block.textPosition === 'Right' ? `order-last` : `order-first`}`} />}
-    {block.image && typeof block.image !== 'string' &&
-      <Image src={block.image.thumbnailURL || ``} alt={block.image.alt || ``} className={'col-span-9'}
-             width={block.image.width || 640} height={block.image.height || 360}
-      />}
+  return <section className={'grid grid-cols-12 gap-4 m-4 p-[6rem] bg-[#afa] rounded-2xl dark:bg-emerald-950 dark:text-emerald-50'}>
+    {block.content
+      && <RichText data={block.content} className={`col-span-12 order-last md:col-span-3 ${block.textPosition === 'Right' 
+        ? `md:order-last` 
+        : `md:order-first`}`}
+      />
+    }
+    {block.image
+      && typeof block.image !== 'string'
+      && <Image
+        src={block.image.thumbnailURL || ``} alt={block.image.alt || ``}
+        className={'col-span-12 md:col-span-9 blur-sm grayscale'}
+        width={block.image.width || 640}
+        height={block.image.height || 360}
+      />
+    }
   </section>
 
 }
