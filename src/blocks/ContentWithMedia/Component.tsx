ed2f41ca61fd7ea42d8b1d97a7de614feb8c4d2a
nick <nick0vogel@gmail.com>
2025-03-22 11:15:10 -0400

    finished

--- src/blocks/ContentWithMedia/Component.tsx
+++ src/blocks/ContentWithMedia/Component.tsx
@@ -1,6 +1,7 @@
 import type { ContentWithMedia as ContentWithMediaProps } from '@/payload-types'
 import Image from 'next/image'
 import { RichText } from '@/components/RichText'
+import {LoaderCircle} from 'lucide-react'
 
 type Props = {
   className?: string
@@ -10,14 +11,15 @@ export const ContentWithMedia: React.FC<Props> = (block) => {
 
   const firstLetter = `first-letter:float-left first-letter:mr-3 first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900`
 
-  return <>
+  return <div className={`print:hidden`}>
     <div className={`flex justify-center p-4`}>
-      <ul role="list" className={`list-disc marker:text-emerald-400 hover:marker:text-emerald-950`}>
+      <ul role="list" className={`sm:grid sm:grid-cols-2 md:grid-cols-3 md:gap-4 list-disc marker:text-emerald-400 hover:marker:text-emerald-950`}>
         <li>Get groceries</li>
         <li>Make dinner</li>
         <li>Stream live coding session</li>
       </ul>
     </div>
+    <LoaderCircle className={'animate-spin motion-reduce:hidden'} />
     <section
       className={'grid grid-cols-12 gap-4 m-4 p-4 bg-[#afa] rounded-2xl dark:bg-emerald-600 dark:text-emerald-50 group'}>
       {block.content
@@ -40,12 +42,12 @@ export const ContentWithMedia: React.FC<Props> = (block) => {
         && <Image
           id={'test'}
           src={block.image.thumbnailURL || ``} alt={block.image.alt || ``}
-          className={'col-span-12 md:col-span-9 group-hover:grayscale'}
+          className={'col-span-12 md:col-span-9 landscape:grayscale'}
           width={block.image.width || 640}
           height={block.image.height || 360}
         />
       }
     </section>
-  </>
+  </div>
 
 }
