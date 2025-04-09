996483d0aafddd25a3a6d03fb30ab05c81a38290
nick <nick0vogel@gmail.com>
2025-04-09 15:01:55 -0400

    tailwind responsive design

--- src/app/(frontend)/tailwind-examples/20250430-responsive-design/page.tsx
+++ src/app/(frontend)/tailwind-examples/20250430-responsive-design/page.tsx
@@ -13,9 +13,11 @@ export default function Page() {
     <div className={`p-2`}>
       <p className={`sm:max-lg:font-bold`}>This won't be bold at the smallest or largest sizes</p>
       {/*Targeting a single breakpoint*/}
-      <p className={`sm:max-md:text-red-700`}>This will only be red at the medium breakpoint</p>
-      {/*Custom breakpoint*/}
-      <p className={`lg:font-bold`}>This will now be bold after 70rem (1120px).</p>
+      <p className={`sm:max-md:text-red-700`}>This will only be red at the small breakpoint</p>
+    </div>
+    {/*Custom breakpoint*/}
+    <div className={`p-2`}>
+      <p className={`sm:font-bold`}>This will now be bold after 36rem (576px).</p>
       {/*Remove and redefine breakpoints*/}
       <p className={`tablet:text-red-400`}>This now uses our custom breakpoints and everything previous ignores
         breakpoint prefixes. This is defined in globals.css.</p>
@@ -26,23 +28,26 @@ export default function Page() {
       <p className={`max-[700px]:font-bold`}>This will be bold until 700px</p>
     </div>
     {/*Container queries, reinstate other breakpoints*/}
-    <p className={`bg-indigo-950 h-16 xl:text-white`}>This is set outside a container and will switch much later because
-      it's relative to the viewport</p>
-    <div className={`@container bg-emerald-950 h-auto w-full`}>
-      <p className={`text-black @xl:text-amber-50`}>This is set within a container and will switch much earlier because
-        it's relative to its container</p>
-      <p className={`@max-xl:text-amber-50`}>This is set to be this color until it reaches the max-xl container
-        size.</p>
-      <p className={`@lg:@max-xl:font-bold text-white`}>We can still use ranges here.</p>
-      {/*Nested containers*/}
-      <div className={`@container/internal`}>
-        <p className={`@2xl/internal:bg-amber-50 @2xl/internal:text-amber-950`}>You can nest containers as well</p>
+    <div className={`p-2`}>
+      <p className={`bg-indigo-950 h-16 xl:text-white`}>This is set outside a container and will switch much later
+        because
+        it's relative to the viewport</p>
+      <div className={`@container bg-emerald-950 h-auto w-full`}>
+        <p className={`text-black @xl:text-amber-50`}>This is set within a container and will switch much earlier
+          because
+          it's relative to its container</p>
+        <p className={`@max-xl:text-amber-50`}>This is set to be this color until it reaches the max-xl container
+          size.</p>
+        <p className={`@lg:@max-xl:font-bold text-white`}>We can still use ranges here.</p>
+        {/*Nested containers*/}
+        <div className={`@container/internal`}>
+          <p className={`@2xl/internal:bg-amber-50 @2xl/internal:text-amber-950`}>You can nest containers as well</p>
+        </div>
+        <p className={`@md:font-bold text-white`}>
+          This should only be bold after 40 rem relative to the container size. This is defined in globals.css.
+        </p>
+        <p className={`@min-[700px]:text-white`}>This is an arbitrary value that applies white text after 700px</p>
       </div>
-      <div className={`@md:font-bold text-white`}><p>
-        This should only be bold after 40 rem relative to the container size. This is defined in globals.css.
-      </p>
-      </div>
-      <p className={`@min-[700px]:text-white`}>This is an arbitrary value that applies white text after 700px</p>
     </div>
     {/*Container query units*/}
     <div className={`@container w-full bg-emerald-500`}>
