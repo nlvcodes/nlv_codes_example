2a05c4aa3b2b5e5b42432c29cc9e54ac7c6703c6
nick <nick0vogel@gmail.com>
2025-05-14 09:32:59 -0400

    make variant check more sleek

--- src/blocks/Column/Component.tsx
+++ src/blocks/Column/Component.tsx
@@ -5,14 +5,17 @@ export const Column = (props: ColumnProps) => {
   const {columnWidth, content} = props
 
   const basisVariant = {
-    'auto': 'lg:basis-auto',
-    '4/5': 'lg:basis-4/5',
-    '3/4': 'lg:basis-3/4',
-    '2/3': 'lg:basis-2/3',
-    '1/2': 'lg:basis-1/2',
-    '1/3': 'lg:basis-1/3',
-    '1/4': 'lg:basis-1/4',
-    '1/5': 'lg:basis-1/5',
+    // this is a less explicit (read: sleeker) way to do the same thing as the commented-out code below
+    [props.columnWidth]: `lg:basis-${columnWidth}`,
+
+    // 'auto': 'lg:basis-auto',
+    // '4/5': 'lg:basis-4/5',
+    // '3/4': 'lg:basis-3/4',
+    // '2/3': 'lg:basis-2/3',
+    // '1/2': 'lg:basis-1/2',
+    // '1/3': 'lg:basis-1/3',
+    // '1/4': 'lg:basis-1/4',
+    // '1/5': 'lg:basis-1/5',
   }
 
   return <div className={`flex flex-col grow basis-full sm:basis-1/2 ${basisVariant[columnWidth]}`}>
